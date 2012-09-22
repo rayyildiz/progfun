8f174cfa628467e15e26396783a708f588cc56c2
rayyildiz <rayyildiz@gmail.com>
2012-09-22 15:28:38 +0300

    pascal trangle and parantheses balancing solved

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -14,12 +14,41 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ??? 
+  def pascal(c: Int, r: Int): Int = {
+    def findAbove(currentRow : Int, currentColumn:Int): Int = {
+      if ( currentColumn == 0 || currentRow == 0 ||  currentColumn == currentRow  ) return 1;
+
+      return findAbove( currentRow-1, currentColumn -1) + findAbove(currentRow-1, currentColumn) ;
+    }
+
+    var result = 1;
+
+    if ( r > 0 && c > 0){
+      if ( c == r) result = 1;
+      else result = findAbove( r-1, c -1) + findAbove(r-1, c);
+    }
+
+    return result;
+  } 
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    var result = false
+
+    if ( chars != null && !chars.isEmpty){
+      var numberOfP = 0;
+      chars.foreach((c: Char) =>{
+        if ( c == '(' && numberOfP >= 0) numberOfP +=1;
+        if ( c == ')') numberOfP -=1;
+      });
+
+      result = (numberOfP == 0);
+    }
+
+    return result;
+  }
 
   /**
    * Exercise 3
