4333efa652155459601198eda1be03d2a8382835
rayyildiz <rayyildiz@gmail.com>
2012-09-23 16:31:29 +0300

    some changes

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -2,6 +2,7 @@ package recfun
 import common._
 import scala.util._
 import scala.collection.mutable._
+import scala.collection.generic.SeqFactory
 
 object Main {
   def main(args: Array[String]) {
@@ -18,18 +19,17 @@ object Main {
    */
   def pascal(c: Int, r: Int): Int = {
     def findAbove(currentRow: Int, currentColumn: Int): Int = {
-      if (currentColumn == 0 || currentRow == 0 || currentColumn == currentRow) return 1
-
-      findAbove(currentRow - 1, currentColumn - 1) + findAbove(currentRow - 1, currentColumn)
+      if (currentColumn == 0 || currentRow == 0 || currentColumn == currentRow) return  1
+      return findAbove(currentRow - 1, currentColumn - 1) + findAbove(currentRow - 1, currentColumn)
     }
 
-    var result = 1
-
+    var result:Int = 1
+    
     if (r > 0 && c > 0) {
       if (c == r) result = 1
       else result = findAbove(r - 1, c - 1) + findAbove(r - 1, c)
     }
-
+    
     result
   }
 
@@ -39,57 +39,60 @@ object Main {
   def balance(chars: List[Char]): Boolean = {
     var result = false
 
-    if (chars != null && !chars.isEmpty) {
+    if (!chars.isEmpty) {
       var numberOfP = 0
       chars.foreach((c: Char) => {
         if (c == '(' && numberOfP >= 0) numberOfP += 1
         if (c == ')') numberOfP -= 1
-      });
+      })
 
       result = (numberOfP == 0)
     }
 
-    return result
+    result
   }
 
   /**
    * Exercise 3
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-    var result = 0
-    
-    def findDivider(number: Int, t1: Int) {
-      println("number: " + number + " t1:" + t1 + "  result:" + result);
-      if (number > 0) {
+    var result: Int = 0
+    if (coins.isEmpty) result
+
+    def findDivider(number: Int, t: Int, x: Int,elements :LinkedList[Int]): Int = {
+      var ret: Int = 0
+
+      println("x:" + x + " number:" + number + " t:" + t + " elements:"+ elements)
+      if (number <= 0) ret;
+      else {
+
         coins.foreach((c: Int) => {
-          var t = number - c;
-         
-          println(">> number:"+number + " c:"+ c + " t:"+t);
-          if (t == 0) {
-            result += 1;
-          } else if (t > 0) {
-            findDivider(t, c);
+          var temp = number - c
+          println(">>>> x:" + x + " number:" + number + " c:" + c)
+
+          if (temp == 0) {
+            //insertInto(elements, c)
+            
+            result += 1
+          } else if (temp > 0) {
+            var xtemp = x + 1
+            findDivider(temp, c, xtemp, elements)
           }
-        });
-     
+        })
       }
-      println("-------------");
+
+      ret
     }
 
-    if (coins == null || coins.isEmpty) return result;
-
+    
     coins.foreach((c: Int) => {
-      var t = money - c;
-      if (t == 0) {
-        result += 1;
-      } else if (t > 0) {
-        findDivider(t, c);
-      }
-      
-    });
+      var temp = (money - c);
+      findDivider(temp, c, 1, new LinkedList)
+    })
 
-    println("Result is " + result);
+    println("result:" + result)
 
-    return result;
+    result
   }
+
 }
